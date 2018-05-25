a36ffc04c6a4f9a721b336fdecd75ebbba8489c3
Paul Spooren <spooren@informatik.uni-leipzig.de>
2018-05-26 02:14:19 +0900

    backup

---
+++ packages/librenet6/files/gsoc/etc/uci-defaults/tinc-gsoc.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+  
+HOSTNAME="$(uci get system.@system[0].hostname)"
+
+# add tinc configuration
+[[ -e /etc/tinc/librenet6/tinc.conf ]] || {
+    mkdir -p /etc/tinc/librenet6/hosts/
+    cat <<EOF >/etc/tinc/librenet6/tinc.conf
+    Name = node_$HOSTNAME
+    Mode = switch
+    ConnectTo = server_gsoc
+EOF
+}
+
